ab7a9b79a22c9b8fa3626dfa28939b15824c278b
Unique Panghal <108991691+panghal007@users.noreply.github.com>
2024-07-09 18:25:57 +0530

    v2: Attachment feature added

--- server.js
+++ server.js
@@ -2,17 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
+
 const emailRoutes = require('./routes/emailRoutes');
 require('dotenv').config();
 require('./config/agenda');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+// Create uploads directory if it does not exist
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(uploadsDir)); // Serve static files from uploads directory
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 app.use('/api', emailRoutes);
 
